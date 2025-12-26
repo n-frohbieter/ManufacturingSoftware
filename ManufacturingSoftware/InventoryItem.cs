19b65cf466945747f70b98355c4814158478ea21
Nathan Frohbieter <nfrohbieter@gmail.com>
2025-12-26 12:04:56 -0600

    Created Classes

---
+++ ManufacturingSoftware/InventoryItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManufacturingSoftware
+{
+    public class InventoryItem
+    {
+        public int IdNumber { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public int QuantityInStock { get; private set; }
+        public decimal PricePerUnit { get; private set; }
+        public string Supplier { get; private set; }
+
+
+        public InventoryItem(int idNumber, string name, string description, int quantityInStock, decimal pricePerUnit, string supplier)
+        {
+            IdNumber = idNumber;
+            Name = name;
+            Description = description;
+            QuantityInStock = quantityInStock;
+            PricePerUnit = pricePerUnit;
+            Supplier = supplier;
+        }
+    }
+}
---
+++ ManufacturingSoftware/Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManufacturingSoftware
+{
+    public class Location
+    {
+        public string Ailse { get; private set; }
+        public string Bay { get; private set; }
+        public string Shelf { get; private set; }
+
+        public Location(string ailse, string bay, string shelf)
+        {
+            Ailse = ailse;
+            Bay = bay;
+            Shelf = shelf;
+        }
+    }
+}
